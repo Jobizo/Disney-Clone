6521f9d2338dcf8ea4673cdac4cdfd9f64925c81
Jobizo <jobbogonko@gmail.com>
2022-10-03 21:32:00 +0300

    First Commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import movieReducer from '../features/movie/movieslice';
+import userSlice from '../features/user/userSlice';
+import userReducer from "../features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    movie: movieReducer,
+    user: userSlice, userReducer
   },
 });
